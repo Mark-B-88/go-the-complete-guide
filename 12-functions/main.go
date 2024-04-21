be37ffe4e886f3b4ac66482925093df0a1e54d69
Marco Vinicio <barrantes_marco@yahoo.com>
2024-04-20 20:01:02 -0600

    returning functions as values

--- 12-functions/main.go
+++ 12-functions/main.go
@@ -6,12 +6,21 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
-
+	moreNumbers := []int{5,1,2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers ,transformerFn1)
+	moreTransformerNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformerNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -21,6 +30,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int { return number * 2 }
